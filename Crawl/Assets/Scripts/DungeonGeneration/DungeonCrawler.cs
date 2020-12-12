3de96475e38e0d25c8fa4fb1bbc1d1bbac3374a6
JonathanKonderla <JonathanKonderla@gmail.com>
2020-12-11 19:18:33 -0600

    can generate rooms

--- Crawl/Assets/Scripts/DungeonGeneration/DungeonCrawler.cs
+++ Crawl/Assets/Scripts/DungeonGeneration/DungeonCrawler.cs
@@ -2,9 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraControler : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
